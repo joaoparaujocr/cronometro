d94f26b959914e5994782335e19ef96fb7ab5070
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2021-10-25 00:32:15 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,101 @@
+const boxButtons = document.querySelector('.box-buttons')
+const buttonPlay = document.getElementById('play');
+const buttonPause = document.getElementById('pause');
+const buttonStop = document.getElementById('stop');
+const spanHour = document.getElementById('hour');
+const spanMinute = document.getElementById('minute');
+const spanSeconds = document.getElementById('seconds');
+const circleSeconds = document.getElementById('progress-seconds');
+const circleMinutes = document.getElementById('progress-minutes');
+const circleHours = document.getElementById('progress-hours');
+
+let hour = 0;
+let minute = 0;
+let seconds = 0;
+let set;
+let progressSeconds = (100 * seconds / 60);
+let progressMinutes = (100 * minute / 60);
+let progressHours = (100 * hour / 24);
+
+circleSeconds.style.strokeDashoffset = 0;
+circleMinutes.style.strokeDashoffset = 0;
+circleHours.style.strokeDashoffset = 0;
+
+
+buttonPlay.addEventListener('click', () => {
+    buttonPlay.parentElement.removeChild(buttonPlay);
+    set = setInterval(() => {
+        timer();
+        progressBars(seconds, minute, hour);
+    }, 1000)
+})
+
+buttonPause.addEventListener('click', () => {
+    boxButtons.insertBefore(buttonPlay, buttonPause);
+    setTimeout(() => {
+        clearInterval(set)
+    })
+})
+
+buttonStop.addEventListener('click', () => {
+    boxButtons.insertBefore(buttonPlay, buttonPause);
+    clearInterval(set);
+    seconds = 0;
+    minute = 0;
+    hour = 0;
+    circleSeconds.style.strokeDashoffset = 0;
+    circleMinutes.style.strokeDashoffset = 0;
+    circleHours.style.strokeDashoffset = 0;
+    spanHour.textContent = '00';
+    spanMinute.textContent = '00';
+    spanSeconds.textContent = '00';
+})
+
+function verificarTimer(val) {
+    return val < 10 ? '0' + val : val;
+}
+
+function timer() {
+    if (seconds < 60) {
+        seconds++;
+        spanSeconds.textContent = verificarTimer(seconds);
+        spanMinute.textContent = verificarTimer(minute);
+    }
+
+    if (seconds === 60) {
+        seconds = 0;
+        minute++;
+        spanSeconds.textContent = verificarTimer(seconds);
+        if (minute === 60) {
+            minute = 0;
+            hour++;
+            spanMinute.textContent = verificarTimer(minute);
+            spanHour.textContent = verificarTimer(hour);
+        }
+        spanMinute.textContent = verificarTimer(minute);
+    }
+}
+
+function progressBars(s, m, h) {
+    progressSeconds = (100 * s / 60);
+    progressMinutes = (100 * m / 60);
+    progressHours = (100 * h / 24);
+    circleSeconds.style.strokeDashoffset = 430 - (widthDocument() * (progressSeconds / 100));
+    circleMinutes.style.strokeDashoffset = 430 - (widthDocument() * (progressMinutes / 100));
+    circleHours.style.strokeDashoffset = 430 - (widthDocument() * (progressHours / 100));
+}
+
+function widthDocument(){
+    const w = getComputedStyle(document.body)
+    const width = w.width;
+    let num = '';
+    for (let i of width) {
+        if (Number(i) || i == 0){
+            num = num + `${i}`
+        }
+    }
+    if (num <= 500) {
+        return 220;
+    }
+    return 380;
+}
